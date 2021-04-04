2c939b6ae29b6405113e8efc09637d303bf199c2
dolphius21 <johnrudolphlee@gmail.com>
2021-04-04 22:11:02 +0800

    done with the ninja list project

---
+++ pages/about.js
@@ -0,0 +1,29 @@
+import Head from 'next/head';
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | Ninja Listing</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum
+          inventore consectetur assumenda minus minima ut fugit quis
+          necessitatibus debitis! Impedit voluptates cum, ullam laboriosam
+          voluptatum eveniet molestias odio magni explicabo!
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum
+          inventore consectetur assumenda minus minima ut fugit quis
+          necessitatibus debitis! Impedit voluptates cum, ullam laboriosam
+          voluptatum eveniet molestias odio magni explicabo!
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
